400811421caee0ff2bc9104d7e194ad804201149
k4y4 <k4y4no@gmail.com>
2026-04-11 00:52:37 +0200

    Router Cars create

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from src.schemas.car_schemas import CarCreate, CarResponse, CarUpdate
 from src.models import car_model
 from src.config.database import Base, engine, get_db
 
+from src.routers import car_router
+
 Base.metadata.create_all(bind=engine)
 
 
@@ -24,86 +26,8 @@ def read_root():
 async def read_item(item_id: int):
     return {"item_id": item_id}
 
-@app.get("/api/cars", response_model=list[CarResponse])
-async def get_all_cars(db: Annotated[Session, Depends(get_db)]):
-    result = db.execute(select(car_model.Car))
-    cars = result.scalars().all()
-    return cars
-
-@app.get("/api/cars/{car_id}", response_model=CarResponse)
-async def get_car(car_id: int, db: Annotated[Session, Depends(get_db)]):
-    result = db.execute(
-        select(car_model.Car).where(car_model.Car.id == car_id)
-    )
-    car = result.scalars().first()
-    if car:
-        return car
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Car not found")
-
-
-@app.put("/api/cars/{car_id}", response_model=CarResponse)
-async def update_car_full(car_id: int, car_data: CarCreate, db: Annotated[Session, Depends(get_db)]):
-    result = db.execute(
-        select(car_model.Car).where(car_model.Car.id == car_id)
-    )
-    car = result.scalars().first()
-    if not car:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Car not found")
-    
-    car.name = car_data.name
-    car.price = car_data.price
-    car.km = car_data.km
-
-    db.commit()
-    db.refresh(car)
-    return car
-
-
-@app.patch("/api/cars/{car_id}", response_model=CarResponse)
-async def update_car_partial(car_id: int, car_data: CarUpdate, db: Annotated[Session, Depends(get_db)]):
-    result = db.execute(
-        select(car_model.Car).where(car_model.Car.id == car_id)
-    )
-    car = result.scalars().first()
-    if not car:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Car not found")
-    
-    updated_data = car_data.model_dump(exclude_unset=True)
-    for field, value in updated_data.items():
-        setattr(car, field, value)
-    
-
-
-    db.commit()
-    db.refresh(car)
-    return car
-
-@app.delete("/api/cars/{car_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_car(car_id: int, db: Annotated[Session, Depends(get_db)]):
-    result = db.execute(
-        select(car_model.Car).where(car_model.Car.id == car_id)
-    )
-    car = result.scalars().first()
-    if not car:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Car not found")
-    
-    db.delete(car)
-    db.commit()
-    
-@app.post(
-    "/api/cars",
-    response_model=CarResponse,
-    status_code=status.HTTP_201_CREATED
+app.include_router(
+    car_router.router,
+    prefix="/api/cars",
+    tags=["Cars"]
 )
-async def create_car(car: CarCreate, db: Annotated[Session, Depends(get_db)]):
-    new_car = car_model.Car (
-        name= car.name,
-        price= car.price,
-        km= car.km
-    )
-
-    db.add(new_car)
-    db.commit()
-    db.refresh(new_car)
-    return new_car
-
---
+++ src/routers/car_router.py
@@ -0,0 +1,97 @@
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, status, Depends
+
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from src.schemas.car_schemas import CarCreate, CarResponse, CarUpdate
+from src.models import car_model
+from src.config.database import Base, engine, get_db
+
+
+router = APIRouter()
+
+@router.get("", response_model=list[CarResponse])
+async def get_all_cars(db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(car_model.Car))
+    cars = result.scalars().all()
+    return cars
+
+@router.get("/{car_id}", response_model=CarResponse)
+async def get_car(car_id: int, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(
+        select(car_model.Car).where(car_model.Car.id == car_id)
+    )
+    car = result.scalars().first()
+    if car:
+        return car
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Car not found")
+
+
+@router.put("/{car_id}", response_model=CarResponse)
+async def update_car_full(car_id: int, car_data: CarCreate, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(
+        select(car_model.Car).where(car_model.Car.id == car_id)
+    )
+    car = result.scalars().first()
+    if not car:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Car not found")
+    
+    car.name = car_data.name
+    car.price = car_data.price
+    car.km = car_data.km
+
+    db.commit()
+    db.refresh(car)
+    return car
+
+
+@router.patch("/{car_id}", response_model=CarResponse)
+async def update_car_partial(car_id: int, car_data: CarUpdate, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(
+        select(car_model.Car).where(car_model.Car.id == car_id)
+    )
+    car = result.scalars().first()
+    if not car:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Car not found")
+    
+    updated_data = car_data.model_dump(exclude_unset=True)
+    for field, value in updated_data.items():
+        setattr(car, field, value)
+    
+
+
+    db.commit()
+    db.refresh(car)
+    return car
+
+@router.delete("/{car_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_car(car_id: int, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(
+        select(car_model.Car).where(car_model.Car.id == car_id)
+    )
+    car = result.scalars().first()
+    if not car:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Car not found")
+    
+    db.delete(car)
+    db.commit()
+    
+@router.post(
+    "",
+    response_model=CarResponse,
+    status_code=status.HTTP_201_CREATED
+)
+async def create_car(car: CarCreate, db: Annotated[Session, Depends(get_db)]):
+    new_car = car_model.Car (
+        name= car.name,
+        price= car.price,
+        km= car.km
+    )
+
+    db.add(new_car)
+    db.commit()
+    db.refresh(new_car)
+    return new_car
+
